8849b0fcd870e14b6490ee89adb009b8bd007e05
Valcinei Jr <hakjog@gmail.com>
2020-11-15 17:55:13 -0300

    first commit

---
+++ src/components/AdBanner/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const AdBanner: React.FC = () => {
+  return (
+    <Container className="banner">
+      <span>Rocketseat - </span>
+      Interface reproduzida em busca do próximo nível.
+    </Container>
+  );
+};
+
+export default AdBanner;
